785d116b6c2e2e0bfd7646df9abf9c2972760626
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-02-27 17:13:27 +0800

    I need to give permissions for instrances to connect to each other

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "master_ssh_command" {
   description = "SSH to master"
-  value       = "Master: ssh -i ~/.ssh/terraform.pem ubuntu@${aws_instance.master.public_ip}"
+  value       = "Master: ssh -i ~/.ssh/terraform.pem -o 'StrictHostKeyChecking no' ubuntu@${aws_instance.master.public_ip}"
 }
 
 output "worker_ssh_command" {
   description = "SSH to worker"
-  value       = { for k, v in aws_instance.worker : k => "Worker: ssh -i ~/.ssh/terraform.pem ubuntu@${v.public_ip}" }
+  value       = { for k, v in aws_instance.worker : k => "Worker: ssh -i ~/.ssh/terraform.pem -o 'StrictHostKeyChecking no' ubuntu@${v.public_ip}" }
 }
